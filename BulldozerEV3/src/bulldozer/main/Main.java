0178521e28ecfaf6c72ef728b141cb3c8f43899b
Nikolay Mladenov <nidenov@gmail.com>
2017-10-24 19:39:17 +0200

    adding Main for testing and small changes in gitingore

---
+++ BulldozerEV3/src/bulldozer/main/Main.java
@@ -0,0 +1,16 @@
+package bulldozer.main;
+
+import lejos.hardware.Button;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Testing...");
+	
+		Button.waitForAnyPress();
+
+	}
+
+}
